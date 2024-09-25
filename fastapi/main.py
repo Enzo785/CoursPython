8e323e8e62ab07c90c714022fa580860b4d9e334
enzo <Vous@exemple.com>
2024-09-25 09:31:10 +0200

    Enzo - 25-09-2024 09:31

--- fastapi/main.py
+++ fastapi/main.py
@@ -16,7 +16,7 @@ def read_root():
 
 @app.get("/enzo")
 def read_enzo():
-    return {"Hello": "Enzo BENVENUTO H3 2024"}
+    return {"Hello": "Test Enzo BENVENUTO H3 2024"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
